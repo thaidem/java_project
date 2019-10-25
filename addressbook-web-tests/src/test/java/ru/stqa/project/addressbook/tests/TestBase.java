ffd61023b5e2f38a286d5789c309d5c39c89dc23
Dmitry Kozlov <thaidem19@gmail.com>
2019-10-25 14:29:47 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.project.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.project.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
 
   @BeforeMethod
