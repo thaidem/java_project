fd179b040784606937fcf22707b648ca061fdf20
Dmitry Kozlov <thaidem19@gmail.com>
2019-11-01 11:41:00 +0300

    Сравнили коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/appmanager/GroupHelper.java
@@ -1,7 +1,12 @@
 package ru.stqa.project.addressbook.appmanager;
 
+import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import ru.stqa.project.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends BaseHelper {
 
@@ -52,5 +57,16 @@ public class GroupHelper extends BaseHelper {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element: elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
   }
 }
