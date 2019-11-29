34b461f2f9385ebc0f12c209edb5461fbdc3892c
Dmitry Kozlov <thaidem19@gmail.com>
2019-11-29 14:51:44 +0300

    Добавлен новый модуль для тестов Mantis

--- addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/tests/HbConnectionTest.java
@@ -41,7 +41,7 @@ public class HbConnectionTest {
     List<ContactData> result = session.createQuery( "from ContactData where deprecated = '0000-00-00'" ).list();
     session.getTransaction().commit();
     session.close();
-    
+
     for ( ContactData contact : result ) {
       System.out.println(contact);
       System.out.println(contact.getGroups());
