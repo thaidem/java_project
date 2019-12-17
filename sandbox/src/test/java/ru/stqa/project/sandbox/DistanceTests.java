8bcabe375c7e9c20e63fa1cd28c73a7b4787fb5b
Dmitry Kozlov <thaidem19@gmail.com>
2019-12-17 15:11:04 +0300

    Испортили тест для проверки Jenkins

--- sandbox/src/test/java/ru/stqa/project/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/project/sandbox/DistanceTests.java
@@ -9,7 +9,7 @@ public class DistanceTests {
   public void testArea() {
 
     Point p1 = new Point(2, 2);
-    Assert.assertEquals(p1.distance(new Point(5, 6)),5.0);
+    Assert.assertEquals(p1.distance(new Point(5, 6)),6.0);
     Point p2 = new Point(5, 6);
     Assert.assertEquals(p2.distance(new Point(10, 12)),7.810249675906654);
     Point p3 = new Point(0, 0);
