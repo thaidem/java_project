7c3f05b4e82779564102046e87b17dfd862a8222
Dmitry Kozlov <thaidem19@gmail.com>
2019-12-18 15:55:13 +0300

    Update ApplicationManager.java

--- addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/appmanager/ApplicationManager.java
@@ -52,7 +52,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
+//      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
