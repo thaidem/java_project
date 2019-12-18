1d0dc3be1e7d143b6badcf59223e8c34fbe956de
Dmitry Kozlov <thaidem19@gmail.com>
2019-12-18 10:57:55 +0300

    Up group

--- addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/project/addressbook/tests/GroupCreationTests.java
@@ -63,14 +63,14 @@ public class GroupCreationTests extends TestBase {
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test(enabled = false)
-  public void testBadGroupCreation() {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test'");
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(before));
-  }
+//  @Test(enabled = false)
+//  public void testBadGroupCreation() {
+//    app.goTo().groupPage();
+//    Groups before = app.group().all();
+//    GroupData group = new GroupData().withName("test'");
+//    app.group().create(group);
+//    assertThat(app.group().count(), equalTo(before.size()));
+//    Groups after = app.group().all();
+//    assertThat(after, equalTo(before));
+//  }
 }
