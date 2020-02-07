47df277d1d695840e923ed8122d6ce86540934c2
Dmitry Kozlov <thaidem19@gmail.com>
2020-02-07 16:23:07 +0300

    ILS-test

---
+++ ILS-test/src/test/java/ru/stqa/project/ils/appmanager/SessionHelper.java
@@ -0,0 +1,16 @@
+package ru.stqa.project.ils.appmanager;
+
+import org.openqa.selenium.By;
+
+public class SessionHelper extends BaseHelper {
+
+  public SessionHelper(ApplicationManager app) {
+    super(app);
+  }
+
+  public void login(String username, String password) {
+    type(By.name("l"), username);
+    type(By.name("p"), password);
+    click(By.xpath("//input[@value='Войти']"));
+  }
+}
---
+++ ILS-test/src/test/java/ru/stqa/project/ils/tests/TestBase.java
@@ -0,0 +1,22 @@
+package ru.stqa.project.ils.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.project.ils.appmanager.ApplicationManager;
+
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+}
